88569dd7298082e252917ce9c7b951bc5109ea90
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-28 07:25:41 +0000

    chore: bump manifest version to v1.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 games { 'gta5' }
 
 name 'prdm_scripts'
-version 'main'
+version '1.0.1'
 description 'Simple scripts to improve quality of life for development'
 author 'i_Zolox & nullpointing'
 
